a3ad39eef384ee97a318d70cfeb92417e0825689
Sam Schwartz <sschwartz222@gmail.com>
2020-05-06 06:18:08 -0500

    just testing out drawing waves upon clicking

--- js/main.js
+++ js/main.js
@@ -1,8 +1,35 @@
 function setup() {
   createCanvas(innerWidth, innerHeight)
-  background(0)
-  let x = width / 2
-  let y = height / 2
-  let r = width / 4
-  ellipse(x, y, r)
+  background(255, 0 , 0)
+
+}
+
+let x = 0
+let i = 0
+let c = 0
+let offsetX = 0
+let offsetY = 0
+
+function draw() {
+  background(255, 0 , 0, 5)
+  if (c===1) {
+    let r = 50
+    let x = i*6 + offsetX
+    let y = sin(i*0.5) * r + offsetY
+    let d = 30
+    ellipse(x,y,d)
+    i+=0.55
+
+  }else{
+    i=0
+    background(255, 0 , 0)
+  }
+  
+}
+
+function mouseClicked(){
+  if (c===0) c = 1
+  else c = 0
+  offsetX = mouseX
+  offsetY = mouseY
 }
